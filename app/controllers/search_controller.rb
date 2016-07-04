a4579064897e4c5ffd374506de466c6db6d2eac7
vrishab <vrishab2494@gmail.com>
2016-07-04 13:06:23 +0530

    working on search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,29 +2,21 @@ class SearchController < ApplicationController
   def index
   	@query = Query.new
   end
+
   def search_result
   	puts params
-  	
-  	respond_to do |format|
-  		format.html{
-  			if params[:query].blank?
-  				@query = params[:querys] 
-  			else
-  				@query = params[:query][:title]
-  			end
-  			@page = params[:page] || 1
-  			@results = GoogleCustomSearchApi.search(@query , page: @page)
-  			puts @results.items.first
-  		}
-  		format.js{}
+  	if params[:query].blank?
+  		@query = params[:querys] 
+  	else
+  		@query = params[:query][:title]
   	end
+  	@page = params[:page] || 1
+  	@results = GoogleCustomSearchApi.search(@query , page: @page)
+  	puts @results.items.first
+  	puts '++++++++++++++++++++++++++++++++'
+  	puts @results.items.first.pagemap.cse_thumbnail.first.src
   end
   def searchs
-  	if params[:q]
-      page = params[:page] || 1
-      @results = GoogleCustomSearchApi.search(params[:q],
-                                              page: page)
-  	end
   end
 
 end
